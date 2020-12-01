002d8dc1e7a6d3c759ac4d316f5ae486905c7dad
lolchek <roman.kercmar@fer.hr>
2020-12-01 21:37:29 +0100

    corrects small mistakes

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,16 @@ fn task1() {
     let filename = "src/day1/data/input.txt";
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     let v: Vec<&str> = contents.split_whitespace().collect();
-    let mut numbers: Vec<i32> = Vec::new();
+    let mut numbers: Vec<i32> = vec![];
     for number in v{
-        numbers.push(number.parse::<i32>().unwrap());
+        numbers.push(number.parse().unwrap());
     }
-    for number in &numbers{
-        for number2 in &numbers{
+    for number in numbers.iter(){
+        for number2 in numbers.iter(){
             if number + number2 == 2020{
                 println!("{}", number*number2);
             }
-            for number3 in &numbers{
+            for number3 in numbers.iter(){
                 if number + number2 + number3 == 2020{
                     println!("{}", number*number2*number3);
                 }
